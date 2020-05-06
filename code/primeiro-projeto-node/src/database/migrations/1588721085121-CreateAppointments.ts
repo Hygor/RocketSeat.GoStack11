7e41f035d614ac85b9db518e6c97502fa91b4d32
Hygor Budny <hygor@me.com>
2020-05-05 21:29:55 -0300

    integrate with database

---
+++ code/primeiro-projeto-node/src/database/migrations/1588721085121-CreateAppointments.ts
@@ -0,0 +1,36 @@
+/* eslint-disable class-methods-use-this */
+import { MigrationInterface, QueryRunner, Table, Column } from 'typeorm';
+
+export default class CreateAppointments1588721085121
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable('appointments');
+  }
+}
